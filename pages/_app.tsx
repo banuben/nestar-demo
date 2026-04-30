4c42ef4ce543e9775f780a096e0a72bd4fec62df
Bonu <bonu2530@gmail.com>
2026-04-30 18:30:05 +0900

    feat: MUI customized integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import React, { useState } from "react";
+import { light } from "../scss/MaterialTheme";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  // @ts-ignore
+  const [theme, setTheme] = useState(createTheme(light));
+  // Socket.io, Redux, Mui ....
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+};
